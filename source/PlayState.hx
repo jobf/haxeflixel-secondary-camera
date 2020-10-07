0e4da496f5c3d6129a28eba2160c39610d57938d
jf <jf>
2020-10-07 12:48:25 +0100

    add some moving sprites

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,38 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
+import flixel.util.FlxSpriteUtil;
 
 class PlayState extends FlxState
 {
+	var planes:Array<FlxSprite>;
+
 	override public function create()
 	{
 		super.create();
+		bgColor = FlxColor.fromRGB(153, 217, 234);
+		planes = [];
+		var numPlanes = 3;
+		var distribution = FlxG.height / numPlanes;
+		for (i in 0...numPlanes)
+		{
+			var plane = new FlxSprite(0, distribution * i, "assets/images/planeYellow3.png");
+			var rng = FlxG.random.int(100, 300);
+			plane.velocity.x = rng;
+			planes.push(plane);
+			add(plane);
+		}
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		for (plane in planes)
+		{
+			FlxSpriteUtil.screenWrap(plane);
+		}
 	}
 }
